dc8466c7751c05ebd0fcb523a14abde9ea3d7c1b
mauroayarde <mauroprogramador157@gmail.com>
2023-05-15 21:54:20 -0300

    Agregar archivos de proyecto.

---
+++ CentralParkingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CentralParkingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
