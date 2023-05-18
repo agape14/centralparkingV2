b02d1462468373cf5de4dde9a21700d7a7e8a18e
fer-br-tiger <fernando.gonzalez.261196@gmail.com>
2023-05-18 13:34:38 -0300

    CRUD Proveedores

--- ApiBD/Controllers/ProveedoresController.cs
+++ ApiBD/Controllers/ProveedoresController.cs
@@ -1,12 +1,89 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiBD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiBD.Controllers
 {
+    [ApiController]
+    [Route("api/proveedor")]
     public class ProveedoresController : Controller
     {
-        public IActionResult Index()
+        private readonly CentralParkingContext _dbContext;
+
+        public ProveedoresController(CentralParkingContext dbContext)
         {
-            return View();
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TbProvRegistro>>> Get()
+        {
+            var proveedores = await _dbContext.TbProvRegistros.ToListAsync();
+            return proveedores;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TbProvRegistro>> GetById(int id)
+        {
+            var proveedor = await _dbContext.TbProvRegistros.FindAsync(id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+            return proveedor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TbProvRegistro>> Create(TbProvRegistro proveedor)
+        {
+            _dbContext.TbProvRegistros.Add(proveedor);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = proveedor.Id }, proveedor);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, TbProvRegistro proveedor)
+        {
+            if (id != proveedor.Id)
+            {
+                return BadRequest();
+            }
+            _dbContext.Entry(proveedor).State = EntityState.Modified;
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProveedorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var proveedor = await _dbContext.TbProvRegistros.FindAsync(id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+            _dbContext.TbProvRegistros.Remove(proveedor);
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool ProveedorExists(int id)
+        {
+            return _dbContext.TbProvRegistros.Any(e => e.Id == id);
         }
     }
 }
