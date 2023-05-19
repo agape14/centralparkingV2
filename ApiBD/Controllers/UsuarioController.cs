3ee9b8a20813a1c425f6f0684cc3a903776e402f
fer-br-tiger <fernando.gonzalez.261196@gmail.com>
2023-05-19 01:09:30 -0300

    CRUD de Usuario

--- ApiBD/Controllers/UsuarioController.cs
+++ ApiBD/Controllers/UsuarioController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 namespace ApiBD.Controllers
 {
     [ApiController]
-    [Route("api/usuarios")]
+    [Route("api/usuario")]
     public class UsuarioController : Controller
     {
         private readonly CentralParkingContext _dbContext;
@@ -16,73 +16,75 @@ namespace ApiBD.Controllers
             _dbContext = dbContext;
         }
 
-
-
-        // GET: Usuario/Details/5
         [HttpGet]
-        public async Task<IActionResult> Details(ulong? id)
+        public async Task<ActionResult<IEnumerable<TbConfUser>>> Get()
         {
-            if (id == null || _dbContext.TbConfUsers == null)
-            {
-                return NotFound();
-            }
-
-            var tbConfUser = await _dbContext.TbConfUsers
-                .Include(t => t.Rol)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (tbConfUser == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(tbConfUser);
+            var usuarios = await _dbContext.TbConfUsers.ToListAsync();
+            return usuarios;
         }
 
-
-
-        // GET: Usuario/Edit/5
-        [HttpPut]
-        public async Task<IActionResult> Edit(ulong? id)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TbConfUser>> GetById(int id)
         {
-            if (id == null || _dbContext.TbConfUsers == null)
+            var usuario = await _dbContext.TbConfUsers.FindAsync(id);
+            if (usuario == null)
             {
                 return NotFound();
             }
-
-            var tbConfUser = await _dbContext.TbConfUsers.FindAsync(id);
-            if (tbConfUser == null)
-            {
-                return NotFound();
-            }
-          
-            return Ok(tbConfUser);
+            return usuario;
         }
 
-
-
-        // GET: Usuario/Delete/5
-        [HttpDelete]
-        public async Task<IActionResult> Delete(ulong? id)
+        [HttpPost]
+        public async Task<ActionResult<TbConfUser>> Create(TbConfUser usuario)
         {
-            if (id == null || _dbContext.TbConfUsers == null)
-            {
-                return NotFound();
-            }
-
-            var tbConfUser = await _dbContext.TbConfUsers
-                .Include(t => t.Rol)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (tbConfUser == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(tbConfUser);
+            _dbContext.TbConfUsers.Add(usuario);
+            await _dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = usuario.Id }, usuario);
         }
 
-      
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(ulong id, TbConfUser usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return BadRequest();
+            }
+            _dbContext.Entry(usuario).State = EntityState.Modified;
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
 
-       
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var usuario = await _dbContext.TbConfUsers.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            _dbContext.TbConfUsers.Remove(usuario);
+            await _dbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+        private bool UsuarioExists(ulong id)
+        {
+            return _dbContext.TbConfUsers.Any(e => e.Id == id);
+        }
 
     }
 }
