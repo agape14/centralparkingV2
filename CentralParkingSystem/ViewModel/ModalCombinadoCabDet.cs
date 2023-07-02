f9fdd38757345452287fcec610c095d6cf6d9f90
mauroayarde <mauroprogramador157@gmail.com>
2023-07-01 22:45:49 -0300

    Clase modelo para mostrar las vistas de los modales de manera dinamica. Programación de servicios en las view de cada modal

---
+++ CentralParkingSystem/ViewModel/ModalCombinadoCabDet.cs
@@ -0,0 +1,10 @@
+﻿using ApiBD.Models;
+
+namespace CentralParkingSystem.ViewModel
+{
+    public class ModalCombinadoCabDet
+    {
+        public TbConfModalcab modalCab { get; set; }
+        public List<TbConfModaldet> listModalDet { get; set; }
+    }
+}
